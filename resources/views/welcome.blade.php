a11a8374905a10f3426f99c7d51def2dabd1667e
FilippoBersan <filippobersan99@gmail.com>
2024-01-29 17:31:26 +0100

    feat: stampa movies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
+    <title>Laravel Model Controller</title>
     @vite('resources/js/app.js')
 </head>
 
@@ -13,11 +13,29 @@
     <div class="container py-5">
         <header>
             <div class="d-flex justify-content-center">
-                <h1>Ciao Classe 110</h1>
+                <h1>Larvel Model Controller</h1>
             </div>
         </header>
 
-        <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="">
+
+
+@foreach ($movies as $movie )
+<div class="
+ d-flex flex-row">
+
+<div class="card ">
+   <h3> {{$movie['title']}}</h3>
+   <strong> {{$movie['original_title']}}</strong>
+   <p> Lingua: {{$movie['nationality']}}</p>
+     <p> Data di uscita: {{$movie['date']}}</p>
+   <p>Voto: {{$movie['vote']}}</p>
+</div>
+
+</div>
+ 
+@endforeach
+
+        {{-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt=""> --}}
     </div>
 
 </body>
